4e36a74f4b185842f040b177d834ad660ce3526f
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2026-06-20 19:48:44 +0700

    refactor: remove unused fields from CkpnWorkpaperForm for cleaner schema

--- app/Filament/Resources/CkpnWorkpapers/Schemas/CkpnWorkpaperForm.php
+++ app/Filament/Resources/CkpnWorkpapers/Schemas/CkpnWorkpaperForm.php
@@ -2,10 +2,8 @@
 
 namespace App\Filament\Resources\CkpnWorkpapers\Schemas;
 
-use App\Models\CkpnWorkpaper;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
@@ -29,31 +27,8 @@ class CkpnWorkpaperForm
                                 fn($query) => $query->where('is_active', true)->orderBy('branch_code'),
                             )
                             ->searchable()
+                            ->required()
                             ->preload(),
-                        Select::make('status')
-                            ->options(CkpnWorkpaper::statusOptions())
-                            ->disabled()
-                            ->dehydrated(false),
-                        TextInput::make('total_receivable_amount')
-                            ->label('Total receivable')
-                            ->disabled()
-                            ->dehydrated(false),
-                        TextInput::make('total_calculated_ckpn_amount')
-                            ->label('Total calculated CKPN')
-                            ->disabled()
-                            ->dehydrated(false),
-                        TextInput::make('total_adjustment_delta')
-                            ->label('Total adjustment delta')
-                            ->disabled()
-                            ->dehydrated(false),
-                        TextInput::make('total_effective_ckpn_amount')
-                            ->label('Total effective CKPN')
-                            ->disabled()
-                            ->dehydrated(false),
-                        TextInput::make('total_ckpn_amount')
-                            ->label('Final CKPN')
-                            ->disabled()
-                            ->dehydrated(false),
                     ]),
             ]);
     }
