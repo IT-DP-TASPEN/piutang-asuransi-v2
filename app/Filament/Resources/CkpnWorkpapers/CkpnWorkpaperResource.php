09f01648afc678a56ef4836b258be770de75c5bd
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2026-07-04 17:13:24 +0700

    feat: Enhance model label retrieval for CKPN workpapers to include dynamic branch and period information

--- app/Filament/Resources/CkpnWorkpapers/CkpnWorkpaperResource.php
+++ app/Filament/Resources/CkpnWorkpapers/CkpnWorkpaperResource.php
@@ -31,11 +31,24 @@ class CkpnWorkpaperResource extends Resource
 
     protected static ?int $navigationSort = 10;
 
-    protected static ?string $modelLabel = 'CKPN workpaper';
+    // protected static ?string $modelLabel = 'CKPN workpaper';
 
     protected static ?string $pluralModelLabel = 'CKPN workpapers';
 
-    protected static ?string $recordTitleAttribute = 'period';
+    public static function getModelLabel(): string
+    {
+        /** @var CkpnWorkpaper|null $record */
+        $record = static::getModel()::query()->first();
+        if (! $record) {
+            return 'CKPN workpaper';
+        }
+
+        return sprintf(
+            'CKPN - %s - %s',
+            $record->branchOffice?->branch_name ?? 'All branches',
+            $record->period->format('d F Y'),
+        );
+    }
 
     public static function getEloquentQuery(): Builder
     {
